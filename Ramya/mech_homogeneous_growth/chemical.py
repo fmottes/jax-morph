Fixed pairwisestress calculation.

Ramya/mech_homogeneous_growth/chemical.py
@@ -20,8 +20,9 @@ def S_fixed_chemfield(istate,
     big_state_output: CellState with updated chemical concentration
   """
   # Find displacements from center of cluster.
-  cluster_box_size = quantity.box_size_at_number_density(params['ncells_init'], 1.2, 2)
-  center = np.array([cluster_box_size/2.0, cluster_box_size/2.0])
+  #cluster_box_size = quantity.box_size_at_number_density(params['ncells_init'], 1.2, 2)
+  #center = np.array([cluster_box_size/2.0, cluster_box_size/2.0])
+  center = np.array([0.0, 0.0])
   chemfield_disp = vmap(fspace.displacement, (0, None))(istate.position, center)
   chemfield_disp = np.linalg.norm(chemfield_disp, axis=1)
   # TODO: Write these out as params

Ramya/mech_homogeneous_growth/divrates.py
@@ -8,11 +8,12 @@ from jax_morph.utils import logistic
 maybe_downcast = util.maybe_downcast
 
 def stress(fspace, state, sigma, epsilon, alpha, r_onset, r_cutoff):
-    energy_fn = energy.morse_pair(fspace.displacement, epsilon=epsilon, alpha=alpha, sigma=sigma, r_onset=r_onset, r_cutoff=r_cutoff)
-    force_fn = quantity.force(energy_fn)
-    forces = force_fn(state.position)
+    energy_fn = energy.morse_pair(fspace.displacement, epsilon=epsilon, alpha=alpha, sigma=sigma, r_onset=r_onset, r_cutoff=r_cutoff, per_particle=True)
+    force_fn = jacrev(energy_fn)
+    forces = -1*force_fn(state.position)
     drs = space.map_product(fspace.displacement)(state.position, state.position)
-    stresses = np.sum(np.multiply(forces, np.sign(drs)), axis=(0, -1))
+    drs = np.transpose(drs, axes=(1, 0, 2))
+    stresses = np.sum(np.multiply(forces, np.sign(drs)), axis=(1, 2))
     stresses = np.where(state.celltype > 0, stresses, 0.0)
     return stresses
 