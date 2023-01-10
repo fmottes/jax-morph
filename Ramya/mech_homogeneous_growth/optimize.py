aa77587bb83099846fa717ebaa282324b0c49af7
gianthawk <rrd1208@gmail.com>
2023-01-10 22:43:37 +0000

    Updated optimization functions.

--- Ramya/mech_homogeneous_growth/optimize.py
+++ Ramya/mech_homogeneous_growth/optimize.py
@@ -4,100 +4,78 @@ import optax
 import equinox as eqx
 from jax_morph.simulation import simulation, sim_trajectory
 
+''' Coefficient of variation of division rates loss.'''
 def cv_divrates(state):
     return np.std(state.divrate)/np.mean(state.divrate)
 
-# does not currently ensemble over different initial states
-def _simple_loss_vk(params, 
+'''Run simulation with given parameters and calculate loss of final state.'''
+@eqx.filter_jit
+@eqx.filter_vmap(default=None, kwargs=dict(sim_key=0))
+def simple_loss(params, 
             hyper_params,
             fstep,
             fspace,
             istate,
-            metric_fn=cv_divrates, 
-            target_metric = 0.,
-            **kwargs
-           ):
-    
-        
-        @eqx.filter_jit
-        def _simple_loss(sim_key=None):
-            '''
-            Only calculates the deterministic part of the square loss, does not manage stochastic nodes.
-            '''
-            # merge params dicts
-            all_params = eqx.combine(params, hyper_params)
-
-            #forward pass - simulation
-            sim_init, sim_step = simulation(fstep, all_params, fspace, **kwargs)
-            fstate, _ = sim_trajectory(istate, sim_init, sim_step, sim_key)
-
-            # Calculate metric of final structure
-            metric_final = metric_fn(fstate)
-
-            # measure difference between final state and target 
-            loss = np.sum(np.power(metric_final - target_metric,2))
-
-            return loss
-        
-        return vmap(_simple_loss)
-
-def avg_simple_loss(params, 
-            hyper_params,
-            fstep,
-            fspace,
-            istate,
-            sim_keys=None,
+            sim_key=None,
             metric_fn=cv_divrates,
-            target_metric = 0.,
-            **kwargs
+            target_metric=0.,
            ):
-    
-    loss_fn = _simple_loss_vk(params, 
-            hyper_params,
-            fstep,
-            fspace,
-            istate,
-            metric_fn,
-            target_metric,
-            **kwargs
-           )
-    
-    
-    losses_ens = loss_fn(sim_key=sim_keys)
-    
-    return np.mean(losses_ens)
 
+    # Merge params dicts.
+    all_params = eqx.combine(params, hyper_params)
+
+    # Forward pass - run simulation.
+    sim_init, sim_step = simulation(fstep, all_params, fspace)
+    fstate, _ = sim_trajectory(istate, sim_init, sim_step, sim_key)
+
+    # Calculate metric of final structure.
+    metric_final = metric_fn(fstate)
+
+    # Measure difference between final state and target. 
+    loss = np.sum(np.power(metric_final - target_metric,2))
+
+    return loss
+
+'''Average loss over a batch of simulations with different seeds.'''
+@eqx.filter_jit #NO JIT IS FASTER IN THIS CASE
+def avg_loss(params, hyper_params, vloss_fn, sim_keys, **kwargs):
+    
+    lss = vloss_fn(params, hyper_params, sim_key=sim_keys, **kwargs)
+    
+    return np.mean(lss)
+
+'''Optimization loop.'''
 def optimize(key, epochs, batch_size, lr, params, train_params, fstep, fspace, istate, **kwargs):
 
+    # Separate params to be optimized.
     p, hp = eqx.partition(params, train_params)
-    optimizer = optax.adam(lr)
+
+    # Set up optimizer.
+    optimizer = optax.chain(optax.clip_by_global_norm(1.0), optax.adam(lr)) 
     opt_state = optimizer.init(p)
 
-    #store initial params
-    params_t = [p]
-    grads_t = []
-
-
-    #store loss at initial params
+    # Generate batch keys.
     key, *batch_subkeys = random.split(key, batch_size+1)
     batch_subkeys = np.array(batch_subkeys)
 
-
-    l, grads = value_and_grad(avg_simple_loss)(p, hp, fstep, fspace, istate, batch_subkeys, **kwargs)
+    # Get starting gradients and loss.
+    l, grads = value_and_grad(avg_loss)(p, hp, simple_loss, batch_subkeys, fstep=fstep, fspace=fspace, istate=istate)
     print("loss: %s" % l)
+    params_t = [p]
     loss_t = [float(l)]
+    grads_t = [grads]
 
-
-
+    # Optimization loop.
     for t in range(epochs):
         
         key, *batch_subkeys = random.split(key, batch_size+1)
         batch_subkeys = np.array(batch_subkeys)
         updates, opt_state = optimizer.update(grads, opt_state, p)
         p = eqx.apply_updates(p, updates)
-        
-        l, grads = value_and_grad(avg_simple_loss)(p, hp, fstep, fspace, istate, batch_subkeys, **kwargs)    
+        l, grads = value_and_grad(avg_loss)(p, hp, simple_loss, batch_subkeys, fstep=fstep, fspace=fspace, istate=istate)
         print("loss: %s" % l)
+        
+        # Store values for each epoch.
         loss_t.append(float(l))
         params_t.append(p)
         grads_t.append(grads)
