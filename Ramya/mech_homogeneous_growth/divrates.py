525d3d306fd9e00ede76d1a8b86e8521dd536498
gianthawk <rrd1208@gmail.com>
2023-01-16 19:51:20 +0000

    Cleaned up mechanical code.

--- Ramya/mech_homogeneous_growth/divrates.py
+++ Ramya/mech_homogeneous_growth/divrates.py
@@ -1,81 +1,41 @@
-from IPython.core.formatters import ForwardDeclaredInstance
-from IPython.core.magics.code import find_source_lines
 import jax.numpy as np
 from jax import jit, lax, vmap, jacrev
-
 import jax_md.dataclasses as jax_dataclasses
 from jax_md import partition, util, smap, space, energy, quantity
 from jax_morph.utils import logistic, polynomial
+from Francesco.chem_twotypes.mechanical import _generate_morse_params_twotypes
 maybe_downcast = util.maybe_downcast
 
-def stress(fspace, state, sigma, epsilon, alpha, r_onset, r_cutoff):
-    # Per-particle morse energies.
-    energy_fn = energy.morse_pair(fspace.displacement, epsilon=epsilon, alpha=alpha, sigma=sigma, r_onset=r_onset, r_cutoff=r_cutoff, per_particle=True)
+def stress(state, params, fspace):
+    """ Calculates stress on each cell."""
+    epsilon_matrix, sigma_matrix = _generate_morse_params_twotypes(state, params)
+    energy_fn = energy.morse_pair(fspace.displacement, epsilon=epsilon_matrix, alpha=params["alpha"], sigma=sigma_matrix, r_onset=params["r_onset"], r_cutoff=params["r_cutoff"], per_particle=True)
     # Removed the minus sign because we want F_ij = force on i (not by i)
     forces = jacrev(energy_fn)(state.position)
-    # Pairwise displacements
-    # So now we have F_ij = force on i by j
-    # and r_ij = displacement from i to j
+    # F_ij = force on i by j, r_ij = displacement from i to j
     drs = space.map_product(fspace.displacement)(state.position, state.position)
-    # Multiply these to get force on particle x direction it's coming from
-    # Note this also masks self-forces
     stresses = np.sum(np.multiply(forces, np.sign(drs)), axis=(0, 2))
-    # Add all components and contributions for each particle.
     stresses = np.where(state.celltype > 0, stresses, 0.0)
     return stresses
 
+# Functions to calculate divrates
 
-def _generate_morse_params_twotypes(state, params):
-    '''
-    Morse interaction params for each particle couple. 
-
-    Returns:
-      sigma_matrix: Distance between particles where the energy has a minimum.
-      epsilon_matrix: Depth of Morse well. 
-    '''
-
-    epsilon_OneOne = params['eps_OneOne']
-    epsilon_TwoTwo = params['eps_TwoTwo']
-    epsilon_OneTwo = params['eps_OneTwo']
+def logistic_divrates(stresses, params):
+    """ Calculates divrates using logistic functions on stress."""
+    divrates = logistic(stresses,params["div_gamma"][0],params["div_k"][0])
+    divrates = np.where(stresses > 0, logistic(stresses,params["div_gamma"][1],params["div_k"][1]), divrates)
+    return divrates 
 
 
-    #minimum energy when the two cells barely touch
-    #minimum of the well is (approx) at the sum of the two radii (sure??)
-    radii = np.array([state.radius]) 
-    sigma_matrix = radii+radii.T
-
-    #calculate epsilon (well depth) for each pair based on type
-    celltypeOne = np.array([np.where(state.celltype==1,1,0)]) 
-    celltypeTwo = np.array([np.where(state.celltype==2,1,0)]) 
-    
-    epsilon_matrix = np.outer(celltypeOne , celltypeOne)* epsilon_OneOne + \
-                   np.outer(celltypeTwo , celltypeTwo)* epsilon_TwoTwo + \
-                   np.outer(celltypeOne , celltypeTwo)* epsilon_OneTwo + \
-                   np.outer(celltypeTwo, celltypeOne)* epsilon_OneTwo 
-
-    return epsilon_matrix, sigma_matrix
-
-def logistic_gr(stresses, params):
-    div = logistic(stresses,params["div_gamma"][0],params["div_k"][0])
-    div = np.where(stresses > 0, logistic(stresses,params["div_gamma"][1],params["div_k"][1]), div)
-    return div
-
-def poly_gr(stresses, params):
-    div = polynomial(stresses, params["div_coeffs"])
-    return div 
 
 def div_mechanical(state, params, fspace, **kwargs) -> np.array:
-    
-    # Calculate stresses
-    epsilon_matrix, sigma_matrix = _generate_morse_params_twotypes(state, params)
-    box_size = quantity.box_size_at_number_density(params['ncells_init'] + params['ncells_add'], 1.2, 2)
-    stresses = stress(fspace, state, sigma_matrix, epsilon_matrix, params["alpha"], params["r_onset"], params["r_cutoff"])
-    
+    """ Calculates divrates only based on stress."""
+    stresses = stress(state, params, fspace)   
     # calculate "rates"
     if "growth_fn" in kwargs:
         div = kwargs["growth_fn"](stresses, params)
     else: 
-        div = logistic_gr(stresses, params)
+        div = logistic_divrates(stresses, params)
     # create array with new divrates
     divrate = np.where(state.celltype>0,div, 0.0)
     max_divrate = logistic(state.field, 0.1, 25.0)
@@ -88,6 +48,7 @@ def div_mechanical(state, params, fspace, **kwargs) -> np.array:
     return divrate
 
 def div_combined(state, params, fspace, **kwargs) -> np.array:
+    """ Calculates divrates based on both stress and chemicals."""
     divrate = div_mechanical(state, params, fspace, **kwargs)
     # Get product of chemical contributions
     vmap_logistic = vmap(logistic, (1,0, 0),(1))
@@ -96,8 +57,9 @@ def div_combined(state, params, fspace, **kwargs) -> np.array:
     divrate = divrate*logistic(state.radius+.06, 50, params['cellRad'])
     return divrate
 
+
 def S_set_divrate(state, params, fspace, divrate_fn=div_mechanical,**kwargs):
-    
+    """ Sets divrates."""
     divrate = divrate_fn(state, params, fspace, **kwargs)
     new_state = jax_dataclasses.replace(state, divrate=divrate)
     
