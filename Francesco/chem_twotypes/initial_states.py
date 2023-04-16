60ed2f5e0f3ed586f6db32a70628c3f961975654
fmottes <francesco.mottes@gmail.com>
2023-04-16 22:44:41 +0200

    rename state vars for clarity

--- Francesco/chem_twotypes/initial_states.py
+++ Francesco/chem_twotypes/initial_states.py
@@ -12,11 +12,11 @@ from jax_morph.mechanics.morse import S_mech_morse_relax
 
 
 
-def init_state_grow(key, onecell_state, params, fspace, n_cells=5):
+def init_state_grow(key, empty_state, params, fspace, n_cells=5):
     '''
-    Initialize a state with a single cell and grow to a given number of cells.
+    Initialize an empty state with a single cell and grow to a given number of cells.
 
-    NOTE: onecell_state must include the following fields for this initialization method to work correctly:
+    NOTE: empty_state must include the following fields for this initialization method to work correctly:
     - position
     - celltype
     - radius
@@ -29,20 +29,20 @@ def init_state_grow(key, onecell_state, params, fspace, n_cells=5):
 
     # elongate datastructures to the accomodate the initial number of cells
     new_fields = {}
-    for field in jdc.fields(onecell_state):
+    for field in jdc.fields(empty_state):
 
         if field.name == 'key':
             new_fields[field.name] = key
 
         else:
             #retrieve the value of the field
-            value = getattr(onecell_state, field.name)
+            value = getattr(empty_state, field.name)
 
             if jax_md.util.is_array(value):
 
                 if len(value.shape) > 0:
                     shape = (n_cells,)+(value.shape[1:])
-                    new_fields[field.name] = np.concatenate([value, np.zeros(shape, dtype=value.dtype)])
+                    new_fields[field.name] = np.zeros(shape, dtype=value.dtype)
                     
                 else:
                     new_fields[field.name] = value
@@ -50,27 +50,26 @@ def init_state_grow(key, onecell_state, params, fspace, n_cells=5):
                 new_fields[field.name] = value
 
 
-    onecell_state = type(onecell_state)(**new_fields)
+    state = type(empty_state)(**new_fields)
 
 
     # initialize the first cell
-    celltype = onecell_state.celltype.at[0].set(1)
-    radius = onecell_state.radius.at[0].set(params['cellRad'])
-    divrate = onecell_state.divrate.at[0].set(1.)
+    celltype = state.celltype.at[0].set(1)
+    radius = state.radius.at[0].set(params['cellRad'])
+    divrate = state.divrate.at[0].set(1.)
 
-    onecell_state = jdc.replace(onecell_state, celltype=celltype, radius=radius, divrate=divrate)
+    state = jdc.replace(state, celltype=celltype, radius=radius, divrate=divrate)
 
     
     # add one cell at a time and relax the system
-    def _init_add(onecell_state, i):
-        onecell_state, _ = S_cell_division(onecell_state, params, fspace)
-        onecell_state = S_grow_cells(onecell_state, params, fspace)
-        onecell_state = S_mech_morse_relax(onecell_state, params, fspace)
-    
-        return onecell_state, 0.
+    def _init_add(state, i):
+        state, _    = S_cell_division(state, params, fspace)
+        state       = S_grow_cells(state, params, fspace)
+        state       = S_mech_morse_relax(state, params, fspace)
+        return state, 0.
     
     iterations = np.arange(n_cells-1)
-    state, _ = lax.scan(_init_add, onecell_state, iterations)
+    state, _ = lax.scan(_init_add, state, iterations)
     
     
     #set all cells to max radius and relax the system
