618547c6cacff3a3a0e52919b8de701c239f4a6a
fmottes <francesco.mottes@gmail.com>
2023-04-16 22:22:35 +0200

    init_state_grow: - set output key to None - dict for new_fields - assert n_cells_add > 0

--- Francesco/chem_twotypes/initial_states.py
+++ Francesco/chem_twotypes/initial_states.py
@@ -5,7 +5,6 @@ from jax import lax
 import jax_md
 import jax_md.dataclasses as jdc
 
-#from jax_morph.datastructures import CellState
 from jax_morph.division_and_growth.cell_division import S_cell_division
 from jax_morph.division_and_growth.cell_growth import S_grow_cells
 from jax_morph.mechanics.morse import S_mech_morse_relax
@@ -15,7 +14,7 @@ from jax_morph.mechanics.morse import S_mech_morse_relax
 
 def init_state_grow(key, onecell_state, params, fspace, n_cells=5):
     '''
-    Initialize a state with a single cell and grow it to a given number of cells.
+    Initialize a state with a single cell and grow to a given number of cells.
 
     NOTE: onecell_state must include the following fields for this initialization method to work correctly:
     - position
@@ -23,12 +22,13 @@ def init_state_grow(key, onecell_state, params, fspace, n_cells=5):
     - radius
     - divrate
 
-    The other fields are initialized to zero. All cells are set to the same radius and celltype.
+    All other fields are initialized to zero. All cells are set to the same radius and celltype.
     '''
 
-    # elongate datastructures to the accomodate the initial number of cells
+    assert n_cells > 0, 'Must initialize at least one cell.'
 
-    new_fields = []
+    # elongate datastructures to the accomodate the initial number of cells
+    new_fields = {}
     for field in jdc.fields(onecell_state):
 
         if field.name == 'key':
@@ -42,35 +42,26 @@ def init_state_grow(key, onecell_state, params, fspace, n_cells=5):
 
                 if len(value.shape) > 0:
                     shape = (n_cells,)+(value.shape[1:])
-                    new_fields.append(np.concatenate([value, np.zeros(shape, dtype=value.dtype)]))
+                    new_fields[field.name] = np.concatenate([value, np.zeros(shape, dtype=value.dtype)])
                     
                 else:
-                    new_fields.append(value)
+                    new_fields[field.name] = value
             else:
-                new_fields.append(value)
-
-    onecell_state = type(onecell_state)(*new_fields)
+                new_fields[field.name] = value
 
 
-    # OLD NAIIVE IMPLEMENTATION
-    #not accountin for the possible presence of HYPERPARAMETERS
-    #new_fields = [np.concatenate([i, np.zeros((n_cells,)+(i.shape[1:]))]) for i in jax.tree_leaves(onecell_state)[:-1]]
+    onecell_state = type(onecell_state)(**new_fields)
 
-        
 
     # initialize the first cell
     celltype = onecell_state.celltype.at[0].set(1)
     radius = onecell_state.radius.at[0].set(params['cellRad'])
-
-
     divrate = onecell_state.divrate.at[0].set(1.)
-
-
 
     onecell_state = jdc.replace(onecell_state, celltype=celltype, radius=radius, divrate=divrate)
 
     
-
+    # add one cell at a time and relax the system
     def _init_add(onecell_state, i):
         onecell_state, _ = S_cell_division(onecell_state, params, fspace)
         onecell_state = S_grow_cells(onecell_state, params, fspace)
@@ -87,33 +78,16 @@ def init_state_grow(key, onecell_state, params, fspace, n_cells=5):
     state = jdc.replace(state, radius=radius)
     
     state = S_mech_morse_relax(state, params, fspace)
+
+    #set key to None to signal possibly inconsistent state
+    state = jdc.replace(state, key=None)
     
     return state
 
 
 
 
-#### NOT FLEXIBLE ENOUGH
-#### SET DEFAULTS IN CELLSTATE DEFINITION INSTEAD
 
 # def _create_onecell_state(key, params):
-    
-#     N = np.int16(params['ncells_init'])
-    
-#     celltype = np.zeros(N, dtype=np.int16)
-#     celltype = celltype.at[0].set(1)
-    
-#     radius = np.zeros(N, dtype=np.float32)
-#     radius = radius.at[0].set(params['cellRad'])
-    
-#     position = np.zeros((N,2), dtype=np.float32)
-    
-#     chemical = np.zeros((N,params['n_chem']), dtype=np.float32)
-#     field = np.zeros(N, dtype=np.float32)
-#     divrate = np.zeros(N, dtype=np.float32)
-#     divrate = divrate.at[0].set(1.)
-    
-#     stress = np.zeros(N, dtype=np.float32)
-#     onec_state = CellState(position, celltype, radius, chemical, field, divrate, stress, key)
-    
-#     return onec_state
+#### NOT FLEXIBLE ENOUGH
+#### SET DEFAULTS IN CELLSTATE DEFINITION INSTEAD
