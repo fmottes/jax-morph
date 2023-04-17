b4b84c8c7028d0365bea73f1777009804045a7d0
fmottes <francesco.mottes@gmail.com>
2023-04-17 15:20:30 +0200

    add _maybe_array

--- jax_morph/utils.py
+++ jax_morph/utils.py
@@ -1,5 +1,15 @@
 import jax.numpy as np
 
+
+#equinox automatically treats as static all non-array values
+#cast floats/ints into arrays if the parameter needs a gradient
+def _maybe_array(name, value, train_params):
+    if train_params[name]:
+        return np.array(value)
+    else:
+        return value
+
+
 # Logistic function 
 def logistic(x,gamma,k):
     return 1./(1.+np.exp(-gamma*(x-k)))
