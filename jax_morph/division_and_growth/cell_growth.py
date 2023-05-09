4bf2e54df45544eb1c4b2e0661e7e24a58a7c9f5
fmottes <francesco.mottes@gmail.com>
2023-05-09 18:07:45 +0200

    default linear growth

--- jax_morph/division_and_growth/cell_growth.py
+++ jax_morph/division_and_growth/cell_growth.py
@@ -2,10 +2,13 @@ import jax.numpy as np
 import jax_md.dataclasses as jdc
 
 
-def S_grow_cells(state, params, fspace=None, grate=.05):
+def S_grow_cells(state, params, fspace=None, grate=.02, type='linear'):
     
     #constant growth
-    new_radius = state.radius * np.exp(grate)
+    if type=='linear':
+        new_radius = state.radius + grate
+    elif type=='exponential':
+        new_radius = state.radius * np.exp(grate)
     
     #set max radius
     new_radius = np.where(new_radius<params['cellRad'], new_radius, params['cellRad'])
