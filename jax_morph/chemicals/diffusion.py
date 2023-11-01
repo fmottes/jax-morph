4845f15933cb8a9004965e04cccded4dfb95390f
fmottes <francesco.mottes@gmail.com>
2023-11-01 17:37:40 -0400

    diffusion steady state with discrete laplacian

--- jax_morph/chemicals/diffusion.py
+++ jax_morph/chemicals/diffusion.py
@@ -1,12 +1,12 @@
 import jax.numpy as np
-from jax import jit, lax, vmap
+from jax import jit, vmap
 
 from jax_md import space, energy
 import jax_md.dataclasses as jdc
 
 
 
-def diffuse_onechem_ss(r,secRate,degRate,diffCoeff):
+def diffuse_onechem_ss_exp(r,secRate,degRate,diffCoeff):
     '''
     NOTE: it is assumed that r is a pairwise distance
     '''
@@ -15,11 +15,11 @@ def diffuse_onechem_ss(r,secRate,degRate,diffCoeff):
     return diff
 
 
-def diffuse_allchem_ss(secretions, state, params, fspace):
+def diffuse_allchem_ss_exp(secretions, state, params, fspace):
 
-    diff = energy.multiplicative_isotropic_cutoff(diffuse_onechem_ss,
-                                                  r_onset = params['r_onsetDiff'],
-                                                  r_cutoff = params['r_cutoffDiff'])
+    # diff = energy.multiplicative_isotropic_cutoff(diffuse_onechem_ss,
+    #                                               r_onset = params['r_onsetDiff'],
+    #                                               r_cutoff = params['r_cutoffDiff'])
     
     metric = space.metric(fspace.displacement)
     d = space.map_product(metric)
@@ -47,6 +47,45 @@ def diffuse_allchem_ss(secretions, state, params, fspace):
     new_chem = np.hstack(new_chem)
     
     return new_chem
+
+
+
+
+
+def diffuse_allchem_ss(secretions, state, params, fspace):
+    
+    #calculate all pairwise distances
+    dist = space.map_product(space.metric(fspace.displacement))(state.position, state.position)
+
+    #prevent division by zero
+    dist *= np.where(np.outer(state.celltype, state.celltype)>0, 1, -1)
+    dist -= np.eye(dist.shape[0])
+
+    #adjacency matrix
+    # zero out connections to inexistent cells
+    A = 1/dist
+    A = (np.where(A>0, A, 0))**2
+
+
+    #graph laplacian
+    L = np.diag(np.sum(A, axis=0)) - A
+
+
+    def ss_chemfield(P, D, K):
+
+        #update laplacian
+        L = D*L + K*np.eye(L.shape[0])
+
+        #solve for steady state
+        x = np.linalg.solve(L, P)
+
+        return x
+
+    new_chem = vmap(ss_chemfield, in_axes=(0, 0, 1), out_axes=(1,))(params['diffCoeff'], params['degRate'], secretions)
+
+    return new_chem
+
+
 
 
 def S_chem_diffusion(state, params, fspace=None, diffusion_fn=diffuse_allchem_ss):
