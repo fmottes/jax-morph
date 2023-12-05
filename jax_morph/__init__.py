0005aa946106b78d0f94d3abd018d13796d43c8d
fmottes <francesco.mottes@gmail.com>
2023-12-05 10:46:01 -0500

    fix visualization import

--- jax_morph/__init__.py
+++ jax_morph/__init__.py
@@ -5,7 +5,7 @@ from . import simulation as simulation
 from . import cell as cell
 from . import env as env
 from . import utils as utils
-from . import utils as utils
+from . import visualization as visualization
 
 from .simulation import SimulationStep, BaseCellState, Sequential
 
