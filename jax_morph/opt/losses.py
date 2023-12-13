9544376c33091103f6cf503682aa181dde4d7dc8
fmottes <francesco.mottes@gmail.com>
2023-12-13 17:58:05 -0500

    add option for validation episodes

--- jax_morph/opt/losses.py
+++ jax_morph/opt/losses.py
@@ -8,32 +8,53 @@ import equinox as eqx
 from ..simulation import simulate
 
 from functools import partial
+from collections import namedtuple
 
 
+# every loss function should return a Loss object
+Loss = namedtuple('Loss', ['loss_fn', 'has_aux'])
 
-def ReinforceLoss(cost_fn, *, n_sim_steps, n_episodes=1, gamma=.9, lambda_l1=0., normalize_cost_returns=True):
+
+def ReinforceLoss(cost_fn, *, n_sim_steps, n_episodes=1, n_val_episodes=0, gamma=.9, lambda_l1=0., normalize_cost_returns=True):
 
     n_episodes = int(n_episodes)
+    n_val_episodes = int(n_val_episodes)
     gamma = float(gamma)
     lambda_l1 = float(lambda_l1)
     normalize_cost_returns = bool(normalize_cost_returns)
     n_sim_steps = int(n_sim_steps)
 
 
-    def _reinforce_loss(model, istate, *, key, n_sim_steps=n_sim_steps, **kwargs):
-
-        subkeys = jax.random.split(key, n_episodes)
-        subkeys = np.asarray(subkeys)
+    def _reinforce_loss(model, istate, *, key, n_sim_steps=n_sim_steps, n_val_episodes=n_val_episodes, **kwargs):
 
         vsim = jax.vmap(partial(simulate, history=True), (None, None, 0, None))
-        trajectory, logp = vsim(model, istate, subkeys, n_sim_steps)
 
 
-        istate = jtu.tree_map(lambda x: np.repeat(x[None,None,:,:],n_episodes,0), istate)
+        key, *subkeys = jax.random.split(key, n_episodes+1)
+        trajectory, logp = vsim(model, istate, np.asarray(subkeys), n_sim_steps)
 
-        trajectory = jtu.tree_map(lambda *v: np.concatenate(v,1), *[istate, trajectory])
+
+        #add istate to beginning of trajectory
+        _istate = jtu.tree_map(lambda x: np.repeat(x[None,None,:,:],n_episodes,0), istate)
+        trajectory = jtu.tree_map(lambda *v: np.concatenate(v,1), *[_istate, trajectory])
 
         cost = jax.vmap(cost_fn)(trajectory)
+
+
+        #calculate actual cost for validation
+        if n_val_episodes > 0:
+
+            n_val_episodes = int(n_val_episodes)
+
+            key, *subkeys = jax.random.split(key, n_val_episodes+1)
+            val_trajectory, _ = vsim(model, istate, np.asarray(subkeys), n_sim_steps)
+
+            #add istate to beginning of val_trajectory
+            _istate = jtu.tree_map(lambda x: np.repeat(x[None,None,:,:],n_val_episodes,0), istate)
+            val_trajectory = jtu.tree_map(lambda *v: np.concatenate(v,1), *[_istate, val_trajectory])
+
+            val_cost = jax.vmap(cost_fn)(val_trajectory).sum(-1).mean()
+
 
 
         #discounted costs
@@ -66,9 +87,12 @@ def ReinforceLoss(cost_fn, *, n_sim_steps, n_episodes=1, gamma=.9, lambda_l1=0.,
             loss = loss + reg
         
 
-        return loss
+        if n_val_episodes > 0:
+            return loss, val_cost
+        else:
+            return loss
 
-    return _reinforce_loss
+    return Loss(loss_fn=_reinforce_loss, has_aux=(n_val_episodes > 0))
     
 
 
