058b28e3ed7d628b5cd436f8330f94faac7a4882
rrd1208@gmail.com <rrd1208@gmail.com>
2025-05-06 19:30:12 -0400

    importing some more things in init

--- jax_morph/cell/__init__.py
+++ jax_morph/cell/__init__.py
@@ -1,6 +1,6 @@
 from . import sensing, gene_networks, mlp, others
 
 from .sensing import LocalChemicalGradients, LocalMechanicalStress
-from .gene_networks import GeneNetwork
+from .gene_networks import GeneNetwork, GeneNetwork_ctype
 from .mlp import CellStateMLP, DivisionMLP, SecretionMLP, HiddenStateMLP
 from .others import SecretionMaskByCellType, AdhesionMaskByCellType
